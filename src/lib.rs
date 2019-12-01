b7fd3352ea82df23db14067d858e61c5914cba62
cssivision <cssivision@gmail.com>
2019-12-01 23:08:26 +0800

    refactor resolver

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
+pub mod resolver;
 pub mod args;
 pub mod cipher;
 pub mod config;
 pub mod io;
-pub mod resolver;
 pub mod socks5;
 #[macro_use]
 pub mod util;
--- src/resolver.rs
+++ src/resolver.rs
@@ -1,70 +1,26 @@
 use std::io;
-use std::mem;
-use std::net::IpAddr;
+use std::net::SocketAddr;
 
-use crate::util::other;
-
-use futures01::{future, Future};
-use tokio01::runtime::current_thread::Runtime;
 use trust_dns_resolver::AsyncResolver;
 use lazy_static::lazy_static;
+use tokio;
 
 lazy_static! {
-    // First we need to setup the global Resolver
+    // setup the global Resolver
     static ref GLOBAL_DNS_RESOLVER: AsyncResolver = {
-        use std::sync::{Arc, Mutex, Condvar};
-        use std::thread;
-
-        // We'll be using this condvar to get the Resolver from the thread...
-        let pair = Arc::new((Mutex::new(None::<AsyncResolver>), Condvar::new()));
-        let pair2 = pair.clone();
-
-        // Spawn the runtime to a new thread...
-        //
-        // This thread will manage the actual resolution runtime
-        thread::spawn(move || {
-            // A runtime for this new thread
-            let mut runtime = Runtime::new().expect("failed to launch Runtime");
-            let (resolver, bg) = AsyncResolver::from_system_conf().expect("Failed to create ResolverFuture");
-
-            let &(ref lock, ref cvar) = &*pair2;
-            let mut started = lock.lock().unwrap();
-            *started = Some(resolver);
-            cvar.notify_one();
-            drop(started);
-
-            runtime.block_on(bg).expect("Fail to create DNS resolver");
-        });
-
-        // Wait for the thread to start up.
-        let &(ref lock, ref cvar) = &*pair;
-        let mut resolver = lock.lock().unwrap();
-        while resolver.is_none() {
-            resolver = cvar.wait(resolver).unwrap();
-        }
-
-        // take the started resolver
-        let resolver = mem::replace(&mut *resolver, None);
-
-        // set the global resolver
-        resolver.expect("resolver should not be none")
+        let (resolver, bg) = AsyncResolver::from_system_conf().expect("Failed to create AsyncResolver");
+        tokio::spawn(bg);
+        resolver
     };
 }
 
-fn resolve01(host: &str) -> impl Future<Item = IpAddr, Error = io::Error> + Send {
-    GLOBAL_DNS_RESOLVER.lookup_ip(host).then(|res| match res {
+async fn resolve(host: &str) -> io::Result<SocketAddr> {
+    match GLOBAL_DNS_RESOLVER.lookup_ip(host).await {
         Ok(r) => if let Some(addr) = r.iter().next() {
-            future::ok(addr)
+            Ok(addr)
         } else {
-            future::err(other("no ip return"))
+            Err(other("no ip return"))
         },
-        Err(e) => future::err(other(&format!("resolve fail: {:?}", e)))
-    })
-}
-
-use futures::compat::Future01CompatExt;
-
-pub async fn resolve(host: &str) -> Result<IpAddr, io::Error> {
-    let v = resolve01(host).compat().await?;
-    Ok(v)
+        Err(e) => Err(other(&format!("resolve fail: {:?}", e)))
+    }
 }
