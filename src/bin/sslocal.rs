6fcb0d5e40b246a63fef3193607f1e2293647dd0
cssivision <cssivision@gmail.com>
2019-12-04 22:25:56 +0800

    refactor sslocal

--- src/bin/sslocal.rs
+++ src/bin/sslocal.rs
@@ -1,97 +1,91 @@
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str::FromStr;
+use std::io::Error;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
-// use log::debug;
+use log::{debug, error};
 
-// use shadowsocks_rs as shadowsocks;
-// use self::shadowsocks::args::parse_args;
-// use self::shadowsocks::cipher::Cipher;
-// use self::shadowsocks::io::{decrypt_copy, encrypt_copy, write_all};
-// use self::shadowsocks::socks5::{
-//     self,
-//     v5::{TYPE_IPV4, TYPE_IPV6, TYPE_DOMAIN},
-// };
+use shadowsocks_rs as shadowsocks;
+use shadowsocks::args::parse_args;
+use shadowsocks::cipher::Cipher;
+use shadowsocks::io::{decrypt_copy, encrypt_copy, write_all};
+use shadowsocks::socks5::{
+    self,
+    v5::{TYPE_IPV4, TYPE_IPV6, TYPE_DOMAIN},
+};
+use shadowsocks::config::Config;
 
-// use futures::{Future, Stream};
-// use tokio::net::{TcpListener, TcpStream};
+use futures::FutureExt;
+use futures::future::try_join;
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
+    let config = parse_args("sslocal").unwrap();
+    println!("{}", serde_json::to_string_pretty(&config).unwrap());
+
+    let mut listener = TcpListener::bind(&config.local_addr).await?;
+    println!("Listening connections on {}", config.local_addr);
+    let cipher = Cipher::new(&config.method, &config.password);
     
+    
+    loop {
+        let config = config.clone();
+        let (socket, _) = listener.accept().await?;
+        let cipher = Arc::new(Mutex::new(cipher.reset()));
+
+        let proxy = proxy(config.clone(), cipher, socket).map(|r| {
+            if let Err(e) = r {
+                error!("failed to proxy; error={}", e);
+            }
+        });
+
+        tokio::spawn(proxy);
+    }
 }
 
-// fn main() {
-//     env_logger::init();
-//     let config = parse_args("sslocal").unwrap();
-//     println!("{}", serde_json::to_string_pretty(&config).unwrap());
+async fn proxy(config: Config, cipher: Arc<Mutex<Cipher>>, mut socket1: TcpStream) -> Result<(u64, u64), Error> {
+    let (host, port) = socks5::serve(&mut socket1).await?;
+    println!("proxy to address: {}:{}", host, port);
 
-//     let local_addr = config.local_addr.parse().expect("invalid local addr");
-//     let listener = TcpListener::bind(&local_addr).unwrap();
-//     let cipher = Cipher::new(&config.method, &config.password);
-//     let server_addr = config.server_addr.parse().expect("invalid server addr");
+    let mut socket2 = TcpStream::connect(&config.server_addr).await?;
+    let rawaddr = generate_raw_addr(&host, port);
+    write_all(cipher.clone(), &mut socket2, &rawaddr).await?;
 
-//     println!("Listening connections on {}", local_addr);
-//     let server = listener
-//         .incoming()
-//         .map_err(|e| eprintln!("accept failed = {:?}", e))
-//         .for_each(move |socket| {
-//             let cipher = Arc::new(Mutex::new(cipher.reset()));
-//             let cipher_copy = cipher.clone();
+    let keepalive_period = config.keepalive_period;
+    let _ = socket1.set_keepalive(Some(Duration::new(keepalive_period, 0)))?;
+    let _ = socket2.set_keepalive(Some(Duration::new(keepalive_period, 0)))?;
 
-//             let pair = socks5::serve(socket).and_then(move |(c1, host, port)| {
-//                 println!("proxy to address: {}:{}", host, port);
-//                 let rawaddr = generate_raw_addr(&host, port);
-//                 TcpStream::connect(&server_addr)
-//                     .and_then(|c2| write_all(cipher_copy, c2, rawaddr).map(|(c2, _)| (c1, c2)))
-//             });
+    let (mut socket1_reader, mut socket1_writer) = socket1.split();
+    let (mut socket2_reader, mut socket2_writer) = socket2.split();
+    let half1 = encrypt_copy(cipher.clone(), &mut socket1_reader, &mut socket2_writer);
+    let half2 = decrypt_copy(cipher.clone(), &mut socket2_reader, &mut socket1_writer);
 
-//             let keepalive_period = config.keepalive_period;
-//             let pipe = pair.and_then(move |(c1, c2)| {
-//                 let _ = c1.set_keepalive(Some(Duration::new(keepalive_period, 0)));
-//                 let _ = c2.set_keepalive(Some(Duration::new(keepalive_period, 0)));
-//                 let c1 = Arc::new(c1);
-//                 let c2 = Arc::new(c2);
+    let (n1, n2) = try_join(half1, half2).await?;
+    debug!("proxy local => remote: {}, remote => local: {}", n1, n2);
+    Ok((n1, n2))
+}
 
-//                 let half1 = encrypt_copy(c1.clone(), c2.clone(), cipher.clone());
-//                 let half2 = decrypt_copy(c2, c1, cipher.clone());
-//                 half1.join(half2)
-//             });
+fn generate_raw_addr(host: &str, port: u16) -> Vec<u8> {
+    if let Ok(host) = Ipv4Addr::from_str(host) {
+        let mut rawaddr = vec![TYPE_IPV4];
+        rawaddr.extend_from_slice(&host.octets());
+        rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
+        return rawaddr;
+    }
 
-//             let finish = pipe
-//                 .map(|data| {
-//                     debug!(
-//                         "received {} bytes, responsed {} bytes",
-//                         (data.0).0,
-//                         (data.1).0
-//                     )
-//                 }).map_err(|e| println!("error: {}", e));
+    if let Ok(host) = Ipv6Addr::from_str(host) {
+        let mut rawaddr = vec![TYPE_IPV6];
+        rawaddr.extend_from_slice(&host.octets());
+        rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
+        return rawaddr;
+    }
 
-//             tokio::spawn(finish);
-//             Ok(())
-//         });
-
-//     tokio::run(server);
-// }
-
-// fn generate_raw_addr(host: &str, port: u16) -> Vec<u8> {
-//     if let Ok(host) = Ipv4Addr::from_str(host) {
-//         let mut rawaddr = vec![TYPE_IPV4];
-//         rawaddr.extend_from_slice(&host.octets());
-//         rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
-//         return rawaddr;
-//     }
-
-//     if let Ok(host) = Ipv6Addr::from_str(host) {
-//         let mut rawaddr = vec![TYPE_IPV6];
-//         rawaddr.extend_from_slice(&host.octets());
-//         rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
-//         return rawaddr;
-//     }
-
-//     let dm_len = host.as_bytes().len();
-//     let mut rawaddr = vec![TYPE_DOMAIN, dm_len as u8];
-//     rawaddr.extend_from_slice(host.as_bytes());
-//     rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
-//     return rawaddr;
-// }
+    let dm_len = host.as_bytes().len();
+    let mut rawaddr = vec![TYPE_DOMAIN, dm_len as u8];
+    rawaddr.extend_from_slice(host.as_bytes());
+    rawaddr.extend_from_slice(&[((port >> 8) & 0xff) as u8, (port & 0xff) as u8]);
+    return rawaddr;
+}
--- src/socks5.rs
+++ src/socks5.rs
@@ -1,15 +1,11 @@
-//! A simple socks5 proxy library.
 use std::io;
 use std::net::{Ipv4Addr, Ipv6Addr};
 use std::str;
-use std::time::Duration;
 
-// use futures::{future, Future};
-// use tokio::net::TcpStream;
-// use tokio_io::io::{read_exact, write_all};
-// use tokio_timer::Timeout;
+use crate::util::other;
 
-// use crate::util::other;
+use tokio::net::TcpStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub mod v5 {
     pub const VERSION: u8 = 5;
@@ -20,117 +16,106 @@ pub mod v5 {
     pub const TYPE_DOMAIN: u8 = 3;
 }
 
-// /// Creates a future which will handle socks5 connection.
-// ///
-// /// if success The returned future will the handled `TcpStream` and address. if handle shake fail will
-// /// return `io::Error`.
-// pub fn serve(
-//     conn: TcpStream,
-// ) -> impl Future<Item = (TcpStream, String, u16), Error = io::Error> + Send {
-//     // socks version, only support version 5.
-//     let version = read_exact(conn, [0u8; 2]).and_then(|(conn, buf)| {
-//         if buf[0] == v5::VERSION {
-//             Ok((conn, buf))
-//         } else {
-//             Err(other("unknown version"))
-//         }
-//     });
+/// Creates a future which will handle socks5 connection.
+///
+/// if success The returned future will the handled `TcpStream` and address. if handle shake fail will
+/// return `io::Error`.
+pub async fn serve(conn: &mut TcpStream) -> io::Result<(String, u16)> {
+    // socks version, only support version 5.
+    let buf1 = &mut [0u8; 2];
+    conn.read_exact(buf1).await?;
 
-//     // ignore socks method
-//     let method = version.and_then(|(conn, buf)| read_exact(conn, [0u8, buf[1] as u8]));
+    if buf1[0] != v5::VERSION {
+        return Err(other("unknown version"));
+    }
 
-//     // send confirmation: version 5, no authentication required
-//     let part1 = method.and_then(|(conn, _)| write_all(conn, [v5::VERSION, v5::METH_NO_AUTH]));
+    let buf2 = &mut [0u8, buf1[1] as u8];
+    conn.read_exact(buf2).await?;
 
-//     // check version
-//     let ack = part1.and_then(|(conn, _)| {
-//         read_exact(conn, [0u8]).and_then(|(conn, buf)| {
-//             if buf[0] == v5::VERSION {
-//                 Ok(conn)
-//             } else {
-//                 Err(other("didn't confirm with v5 version"))
-//             }
-//         })
-//     });
-//     // checkout cmd
-//     let command = ack.and_then(|conn| {
-//         read_exact(conn, [0u8]).and_then(|(conn, buf)| {
-//             if buf[0] == v5::CMD_CONNECT {
-//                 Ok(conn)
-//             } else {
-//                 Err(other("unsupported command"))
-//             }
-//         })
-//     });
+    let buf3 = &mut [v5::VERSION, v5::METH_NO_AUTH]; 
+    conn.write_all(buf3).await?;
 
-//     // there's one byte which is reserved for future use, so we read it and discard it.
-//     let resv = command.and_then(|c| read_exact(c, [0u8]));
-//     let adress_type = resv.and_then(|(conn, _)| read_exact(conn, [0u8]));
-//     let address = adress_type.and_then(move |(c, buf)| {
-//         match buf[0] {
-//             // For IPv4 addresses, we read the 4 bytes for the address as
-//             // well as 2 bytes for the port.
-//             v5::TYPE_IPV4 => mybox(read_exact(c, [0u8; 6]).and_then(|(c, buf)| {
-//                 let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
-//                 let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
-//                 mybox(future::ok((c, format!("{}", addr), port)))
-//             })),
+    // check version
+    let version = &mut [0u8];
+    conn.read_exact(version).await?;
+    if version[0] != v5::VERSION {
+        return Err(other("didn't confirm with v5 version"));
+    }
 
-//             // For IPv6 addresses there's 16 bytes of an address plus two
-//             // bytes for a port, so we read that off and then keep going.
-//             v5::TYPE_IPV6 => mybox(read_exact(c, [0u8; 18]).and_then(|(conn, buf)| {
-//                 let a = ((buf[0] as u16) << 8) | (buf[1] as u16);
-//                 let b = ((buf[2] as u16) << 8) | (buf[3] as u16);
-//                 let c = ((buf[4] as u16) << 8) | (buf[5] as u16);
-//                 let d = ((buf[6] as u16) << 8) | (buf[7] as u16);
-//                 let e = ((buf[8] as u16) << 8) | (buf[9] as u16);
-//                 let f = ((buf[10] as u16) << 8) | (buf[11] as u16);
-//                 let g = ((buf[12] as u16) << 8) | (buf[13] as u16);
-//                 let h = ((buf[14] as u16) << 8) | (buf[15] as u16);
-//                 let addr = Ipv6Addr::new(a, b, c, d, e, f, g, h);
-//                 let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
-//                 mybox(future::ok((conn, format!("{}", addr), port)))
-//             })),
+    // checkout cmd
+    let cmd = &mut [0u8];
+    conn.read_exact(cmd).await?;
+    if cmd[0] != v5::CMD_CONNECT {
+        return Err(other("unsupported command"));
+    }
 
-//             // The SOCKSv5 protocol not only supports proxying to specific
-//             // IP addresses, but also arbitrary hostnames.
-//             v5::TYPE_DOMAIN => mybox(
-//                 read_exact(c, [0u8])
-//                     .and_then(|(conn, buf)| read_exact(conn, vec![0u8; buf[0] as usize + 2]))
-//                     .and_then(|(conn, buf)| {
-//                         let hostname = &buf[..buf.len() - 2];
-//                         let hostname = if let Ok(hostname) = str::from_utf8(hostname) {
-//                             hostname
-//                         } else {
-//                             return mybox(future::err(other("hostname include invalid utf8")));
-//                         };
+    // there's one byte which is reserved for future use, so we read it and discard it.
+    conn.read_exact(&mut [0u8]).await?;
+    let address_type = &mut [0u8]; 
+    conn.read_exact(address_type).await?;
 
-//                         let pos = buf.len() - 2;
-//                         let port = ((buf[pos] as u16) << 8) | (buf[pos + 1] as u16);
-//                         mybox(future::ok((conn, hostname.to_string(), port)))
-//                     }),
-//             ),
-//             n => {
-//                 let msg = format!("unknown address type, received: {}", n);
-//                 mybox(future::err(other(&msg)))
-//             }
-//         }
-//     });
+    let result = match address_type[0] {
+        // For IPv4 addresses, we read the 4 bytes for the address as
+        // well as 2 bytes for the port.
+        v5::TYPE_IPV4 => {
+            let buf = &mut [0u8; 6];
+            conn.read_exact(buf).await?;
+            let addr = Ipv4Addr::new(buf[0], buf[1], buf[2], buf[3]);
+            let port = ((buf[4] as u16) << 8) | (buf[5] as u16);
+            Ok((format!("{}", addr), port))
+        },
 
-//     // Sending connection established message immediately to client.
-//     // This some round trip time for creating socks connection with the client.
-//     // But if connection failed, the client will get connection reset error.
-//     let handshake_finish = address.and_then(move |(conn, addr, port)| {
-//         let resp = [0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x08, 0x43];
-//         write_all(conn, resp).map(move |(conn, _)| (conn, addr, port))
-//     });
+        // For IPv6 addresses there's 16 bytes of an address plus two
+        // bytes for a port, so we read that off and then keep going.
+        v5::TYPE_IPV6 => {
+            let buf = &mut [0u8; 18];
+            conn.read_exact(buf).await?;
+            let a = ((buf[0] as u16) << 8) | (buf[1] as u16);
+            let b = ((buf[2] as u16) << 8) | (buf[3] as u16);
+            let c = ((buf[4] as u16) << 8) | (buf[5] as u16);
+            let d = ((buf[6] as u16) << 8) | (buf[7] as u16);
+            let e = ((buf[8] as u16) << 8) | (buf[9] as u16);
+            let f = ((buf[10] as u16) << 8) | (buf[11] as u16);
+            let g = ((buf[12] as u16) << 8) | (buf[13] as u16);
+            let h = ((buf[14] as u16) << 8) | (buf[15] as u16);
+            let addr = Ipv6Addr::new(a, b, c, d, e, f, g, h);
+            let port = ((buf[16] as u16) << 8) | (buf[17] as u16);
+            Ok((format!("{}", addr), port))
+        },
 
-//     let timeout = Timeout::new(handshake_finish, Duration::new(10, 0))
-//         .map_err(|_| other("handshake timeout"));
+        // The SOCKSv5 protocol not only supports proxying to specific
+        // IP addresses, but also arbitrary hostnames.
+        v5::TYPE_DOMAIN => {
+            let buf1 = &mut [0u8];
+            conn.read_exact(buf1).await?;
+            let buf2 = &mut vec![0u8; buf1[0] as usize + 2];
+            conn.read_exact(buf2).await?;
 
-//     timeout
-// }
+            let hostname = &buf2[..buf2.len() - 2];
+            let hostname = if let Ok(hostname) = str::from_utf8(hostname) {
+                hostname
+            } else {
+                return Err(other("hostname include invalid utf8"));
+            };
 
-// fn mybox<F: Future + 'static + Send>(f: F) -> Box<dyn Future<Item = F::Item, Error = F::Error> + Send> {
-//     Box::new(f)
-// }
+            let pos = buf2.len() - 2;
+            let port = ((buf2[pos] as u16) << 8) | (buf2[pos + 1] as u16);
+            Ok((hostname.to_string(), port))
+        },
+        n => {
+            let msg = format!("unknown address type, received: {}", n);
+            Err(other(&msg))
+        }
+    };
+
+    // Sending connection established message immediately to client.
+    // This some round trip time for creating socks connection with the client.
+    // But if connection failed, the client will get connection reset error.
+    let resp = &[0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x08, 0x43];
+    conn.write_all(resp).await?;
+
+    // let timeout = Timeout::new(handshake_finish, Duration::new(10, 0))
+    //     .map_err(|_| other("handshake timeout"));
+
+    result
+}
