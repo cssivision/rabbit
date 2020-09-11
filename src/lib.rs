d7a424935eacdd8b9fcf80622879815093e524f3
cssivision <cssivision@gmail.com>
2020-09-11 16:12:59 +0800

    use macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,19 @@
+macro_rules! ready {
+    ($e:expr $(,)?) => {
+        match $e {
+            std::task::Poll::Ready(t) => t,
+            std::task::Poll::Pending => return std::task::Poll::Pending,
+        }
+    };
+}
+
 pub mod args;
 pub mod cipher;
 pub mod config;
+mod copy;
 pub mod io;
+mod read_exact;
 pub mod resolver;
 pub mod socks5;
-#[macro_use]
 pub mod util;
-mod copy;
-mod read_exact;
 mod write_all;
