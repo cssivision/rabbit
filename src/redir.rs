0103b9750bb11e28ac212b0e299d11b63e89829d
cssivision <cssivision@gmail.com>
2022-07-23 05:34:59 +0000

    add udp redir

--- src/redir.rs
+++ src/redir.rs
@@ -1,8 +1,7 @@
 use std::future::pending;
 use std::future::Future;
-use std::io::{self, Error};
+use std::io;
 use std::net::SocketAddr;
-use std::os::unix::io::AsRawFd;
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll};
@@ -11,7 +10,6 @@ use std::time::Duration;
 use awak::net::{TcpListener, TcpStream, UdpSocket};
 use awak::time::timeout;
 use futures_util::{future::join, AsyncRead, AsyncWrite, Stream};
-use socket2::SockAddr;
 
 use crate::cipher::Cipher;
 use crate::config::{self, Mode};
@@ -19,7 +17,6 @@ use crate::io::{copy_bidirectional, write_all, IdleTimeout, DEFAULT_IDLE_TIMEOUT
 use crate::util::{generate_raw_addr, other};
 
 const DEFAULT_CONNECT_TIMEOUT: Duration = Duration::from_secs(1);
-const DEFAULT_RESLOVE_TIMEOUT: Duration = Duration::from_secs(1);
 const MAX_UDP_BUFFER_SIZE: usize = 65536;
 
 pub struct Server {
@@ -80,7 +77,7 @@ impl Service {
             let original_dst_addr = if let Some(addr) = self.config.redir_addr {
                 addr
             } else {
-                get_original_dst_addr(&socket)?
+                unimplemented!()
             };
             let proxy = async move {
                 if let Err(e) = proxy(server_addr, cipher, &mut socket, original_dst_addr).await {
@@ -95,7 +92,18 @@ impl Service {
         let cipher = Cipher::new(&self.config.method, &self.config.password);
         let socket = UdpSocket::bind(self.config.local_addr)?;
         log::info!("listening udp on {:?}", self.config.local_addr);
-        UdpRelay::new(socket, cipher).await
+        let (sender, receiver) = async_channel::unbounded::<(Vec<u8>, SocketAddr)>();
+        let udp_relay = UdpRelay {
+            buf: [0u8; MAX_UDP_BUFFER_SIZE],
+            sender,
+            receiver,
+            socket,
+            recv: None,
+            cipher,
+            server_addr: self.config.server_addr,
+            redir_addr: self.config.redir_addr,
+        };
+        udp_relay.await
     }
 }
 
@@ -106,21 +114,11 @@ struct UdpRelay {
     socket: UdpSocket,
     recv: Option<(Vec<u8>, SocketAddr)>,
     cipher: Cipher,
+    server_addr: SocketAddr,
+    redir_addr: Option<SocketAddr>,
 }
 
 impl UdpRelay {
-    fn new(socket: UdpSocket, cipher: Cipher) -> UdpRelay {
-        let (sender, receiver) = async_channel::unbounded::<(Vec<u8>, SocketAddr)>();
-        UdpRelay {
-            buf: [0u8; MAX_UDP_BUFFER_SIZE],
-            sender,
-            receiver,
-            socket,
-            recv: None,
-            cipher,
-        }
-    }
-
     fn poll_send_one(&mut self, cx: &Context, data: (Vec<u8>, SocketAddr)) -> Poll<io::Result<()>> {
         match self.socket.poll_send_to(cx, &data.0, data.1) {
             Poll::Pending => {
@@ -157,8 +155,13 @@ impl Future for UdpRelay {
                     buf.copy_from_slice(&me.buf[..n]);
                     let cipher = me.cipher.reset();
                     let sender = me.sender.clone();
+                    let server_addr = me.server_addr;
+                    let redir_addr = me.redir_addr;
                     awak::spawn(async move {
-                        if let Err(e) = proxy_packet(cipher, buf, peer_addr, sender).await {
+                        if let Err(e) =
+                            proxy_packet(server_addr, cipher, buf, peer_addr, redir_addr, sender)
+                                .await
+                        {
                             log::error!("failed to proxy; error={}", e);
                         };
                     })
@@ -170,43 +173,39 @@ impl Future for UdpRelay {
 }
 
 async fn proxy_packet(
-    cipher: Cipher,
+    server_addr: SocketAddr,
+    mut cipher: Cipher,
     mut buf: Vec<u8>,
+    peer_addr: SocketAddr,
+    redir_addr: Option<SocketAddr>,
     sender: async_channel::Sender<(Vec<u8>, SocketAddr)>,
-    original_dst_addr: SocketAddr,
 ) -> io::Result<(u64, u64)> {
-    let cipher = Arc::new(Mutex::new(cipher));
-    let rawaddr = generate_raw_addr(
-        &original_dst_addr.ip().to_string(),
-        original_dst_addr.port(),
-    );
-    write_all(cipher.clone(), &mut socket2, &rawaddr).await?;
-
-    // resolver host if need.
-    let addr = timeout(DEFAULT_RESLOVE_TIMEOUT, resolve(&host)).await??;
-    log::debug!("resolver addr to ip: {}", addr);
-    let local: SocketAddr = if addr.is_ipv4() {
-        ([0u8; 4], 0).into()
+    cipher.init_encrypt();
+    let mut data = cipher.iv().to_vec();
+    let redir_addr = if let Some(redir_addr) = redir_addr {
+        redir_addr
     } else {
-        ([0u16; 8], 0).into()
+        unimplemented!()
     };
+    let rawaddr = generate_raw_addr(&redir_addr.ip().to_string(), redir_addr.port());
+    data.extend_from_slice(&rawaddr);
+    cipher.encrypt(&mut buf);
+    data.extend_from_slice(&buf);
 
-    // decrypt recv data, dec already init in get_addr_info() function.
-    cipher.lock().unwrap().decrypt(&mut buf);
-
+    let local: SocketAddr = ([0u8; 4], 0).into();
     // send to and recv from target.
     let socket = UdpSocket::bind(&local)?;
-    socket.connect((addr, port))?;
+    socket.connect(server_addr)?;
     let _ = socket.send(&buf).await?;
     let mut recv_buf = vec![0u8; MAX_UDP_BUFFER_SIZE];
     let n = socket.recv(&mut recv_buf).await?;
     recv_buf.truncate(n);
 
     // encrypt return data.
-    if !cipher.lock().unwrap().is_encrypt_inited() {
-        cipher.lock().unwrap().init_encrypt();
+    if !cipher.is_decrypt_inited() {
+        cipher.init_decrypt();
     }
-    cipher.lock().unwrap().encrypt(&mut recv_buf);
+    cipher.decrypt(&mut recv_buf);
     sender
         .try_send((recv_buf.to_vec(), peer_addr))
         .map_err(|e| other(&format!("send fail: {}", e)))?;
@@ -240,46 +239,4 @@ where
     .await??;
     log::debug!("proxy local => remote: {}, remote => local: {:?}", n1, n2);
     Ok((n1, n2))
-}
-
-fn get_original_dst_addr(s: &TcpStream) -> io::Result<SocketAddr> {
-    let fd = s.as_raw_fd();
-
-    unsafe {
-        let (_, target_addr) = SockAddr::init(|target_addr, target_addr_len| {
-            match s.local_addr()? {
-                SocketAddr::V4(..) => {
-                    let ret = libc::getsockopt(
-                        fd,
-                        libc::SOL_IP,
-                        libc::SO_ORIGINAL_DST,
-                        target_addr as *mut _,
-                        target_addr_len, // libc::socklen_t
-                    );
-                    if ret != 0 {
-                        let err = Error::last_os_error();
-                        return Err(err);
-                    }
-                }
-                SocketAddr::V6(..) => {
-                    let ret = libc::getsockopt(
-                        fd,
-                        libc::SOL_IPV6,
-                        libc::IP6T_SO_ORIGINAL_DST,
-                        target_addr as *mut _,
-                        target_addr_len, // libc::socklen_t
-                    );
-
-                    if ret != 0 {
-                        let err = Error::last_os_error();
-                        return Err(err);
-                    }
-                }
-            }
-            Ok(())
-        })?;
-
-        // Convert sockaddr_storage to SocketAddr
-        Ok(target_addr.as_socket().expect("SocketAddr"))
-    }
 }
