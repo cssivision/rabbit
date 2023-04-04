4847719dadbeeeef22dbbc61c8badd87b854a2c4
cssivision <cssivision@gmail.com>
2023-04-04 15:44:22 +0800

    return random ip from dns

--- src/resolver.rs
+++ src/resolver.rs
@@ -4,6 +4,7 @@ use std::str::FromStr;
 
 use dns_resolver::Resolver;
 use once_cell::sync::Lazy;
+use rand::{thread_rng, Rng};
 
 use crate::util::other;
 
@@ -13,12 +14,9 @@ pub async fn resolve(host: &str) -> io::Result<IpAddr> {
     if let Ok(addr) = IpAddr::from_str(host) {
         return Ok(addr);
     }
-
     let results = GLOBAL_RESOLVER.lookup_host(host).await?;
-
     if !results.is_empty() {
-        return Ok(results[0]);
+        return Ok(results[thread_rng().gen_range(0..results.len())]);
     }
-
     Err(other("resolve fail"))
 }
