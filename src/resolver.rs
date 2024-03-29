1c6afbd4b9d3094779bbbc87d5332bc1baa95a52
cssivision <cssivision@gmail.com>
2024-03-29 16:02:00 +0800

    use OnceLock

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,21 +1,21 @@
-use std::cell::OnceCell;
 use std::io;
 use std::net::IpAddr;
 use std::str::FromStr;
+use std::sync::OnceLock;
 
 use dns_resolver::Resolver;
 use rand::{thread_rng, Rng};
 
 use crate::util::other;
 
-const GLOBAL_RESOLVER: OnceCell<Resolver> = OnceCell::new();
+static GLOBAL_RESOLVER: OnceLock<Resolver> = OnceLock::new();
 
 pub async fn resolve(host: &str) -> io::Result<IpAddr> {
     if let Ok(addr) = IpAddr::from_str(host) {
         return Ok(addr);
     }
-
     let host = host.to_string();
+
     let results = GLOBAL_RESOLVER
         .get_or_init(Resolver::new)
         .lookup_host(host)
