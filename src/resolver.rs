1bacef2283ab6433cd19914c42899a8000f4d1fe
cssivision <cssivision@gmail.com>
2024-03-29 15:11:26 +0800

    use OnceLock

--- src/resolver.rs
+++ src/resolver.rs
@@ -3,18 +3,18 @@ use std::net::IpAddr;
 use std::str::FromStr;
 
 use dns_resolver::Resolver;
-use once_cell::sync::Lazy;
 use rand::{thread_rng, Rng};
 
 use crate::util::other;
 
-static GLOBAL_RESOLVER: Lazy<Resolver> = Lazy::new(Resolver::new);
+static GLOBAL_RESOLVER: std::sync::OnceLock<Resolver> = std::sync::OnceLock::new();
 
 pub async fn resolve(host: &str) -> io::Result<IpAddr> {
     if let Ok(addr) = IpAddr::from_str(host) {
         return Ok(addr);
     }
-    let results = GLOBAL_RESOLVER.lookup_host(host).await?;
+    let resolver = GLOBAL_RESOLVER.get_or_init(Resolver::new);
+    let results = resolver.lookup_host(host).await?;
     if !results.is_empty() {
         return Ok(results[thread_rng().gen_range(0..results.len())]);
     }
