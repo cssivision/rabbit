c1508aab400a67fdd07c12a7afc36bf1a16aee03
cssivision <cssivision@gmail.com>
2024-03-30 12:59:52 +0000

    optimize resolver

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,14 +1,16 @@
 use std::io;
 use std::net::IpAddr;
+use std::rc::Rc;
 use std::str::FromStr;
-use std::sync::OnceLock;
 
 use dns_resolver::Resolver;
 use rand::{thread_rng, Rng};
 
 use crate::util::other;
 
-static GLOBAL_RESOLVER: OnceLock<Resolver> = OnceLock::new();
+thread_local! {
+    static GLOBAL_RESOLVER: Rc<Resolver> = Rc::new(Resolver::new());
+}
 
 pub async fn resolve(host: &str) -> io::Result<IpAddr> {
     if let Ok(addr) = IpAddr::from_str(host) {
@@ -16,10 +18,8 @@ pub async fn resolve(host: &str) -> io::Result<IpAddr> {
     }
     let host = host.to_string();
 
-    let results = GLOBAL_RESOLVER
-        .get_or_init(Resolver::new)
-        .lookup_host(host)
-        .await?;
+    let resolver = GLOBAL_RESOLVER.with(|r| r.clone());
+    let results = resolver.lookup_host(host).await?;
     if !results.is_empty() {
         return Ok(results[thread_rng().gen_range(0..results.len())]);
     }
